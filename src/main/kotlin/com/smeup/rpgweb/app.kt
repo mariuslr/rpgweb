5438716905bc711ead8f0e006a590a697dab1e78
Franco Lombardo <f.lombardo69@gmail.com>
2019-11-14 13:07:20 +0100

    First API to call RPG programs

--- src/main/kotlin/com/smeup/rpgweb/app.kt
+++ src/main/kotlin/com/smeup/rpgweb/app.kt
@@ -1,10 +1,13 @@
 package com.smeup.rpgweb
 
 import com.google.gson.GsonBuilder
+import com.smeup.rpgparser.execution.getProgram
+import com.smeup.rpgparser.jvminterop.JavaSystemInterface
+import com.smeup.rpgparser.utils.StringOutputStream
 import io.javalin.Javalin
 import io.javalin.http.Context
 import io.javalin.http.Handler
-import java.io.File
+import java.io.PrintStream
 
 fun main(args: Array<String>) {
     val app = Javalin.create{ config ->
@@ -12,18 +15,32 @@ fun main(args: Array<String>) {
         config.enableCorsForAllOrigins()
     }.start(7000)
     app.get("/", HelloWorldHandler)
-    app.get("/run", RunRPGHandler)
-}
-
-object RunRPGHandler : Handler {
-    override fun handle(ctx: Context) {
-        println(File(".").absolutePath)
-    }
+    app.get("/run/:pgmName", RunRPGHandler)
 }
 
 object HelloWorldHandler : Handler {
     override fun handle(ctx: Context) {
-        val gson = GsonBuilder().setPrettyPrinting().create()
-        ctx.result(gson.toJson("Hello world!"))
+        ctx.jsonResponse("Hello world!")
     }
 }
+
+object RunRPGHandler : Handler {
+    override fun handle(ctx: Context) {
+        ctx.jsonResponse(rpgExecution(ctx.pathParam("pgmName")))
+    }
+}
+
+private fun rpgExecution(pgmName: String): List<String> {
+    val logOutputStream = StringOutputStream()
+    val printStream = PrintStream(logOutputStream)
+    val javaSystemInterface = JavaSystemInterface(printStream)
+    val pgm = getProgram("srcRPG/$pgmName", javaSystemInterface)
+    pgm.singleCall(listOf("Franco"))
+    return logOutputStream.toString().lines()
+}
+
+fun Context.jsonResponse(obj: Any) {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+    this.result(gson.toJson(obj))
+}
+
