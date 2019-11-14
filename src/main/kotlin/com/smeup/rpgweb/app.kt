89e4c8d653704f2acf17c7e983ad9f424d2ce784
Franco Lombardo <f.lombardo69@gmail.com>
2019-11-14 15:01:15 +0100

    RPG programs can have 9 string parameters

--- src/main/kotlin/com/smeup/rpgweb/app.kt
+++ src/main/kotlin/com/smeup/rpgweb/app.kt
@@ -5,42 +5,42 @@ import com.smeup.rpgparser.execution.getProgram
 import com.smeup.rpgparser.jvminterop.JavaSystemInterface
 import com.smeup.rpgparser.utils.StringOutputStream
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.path
 import io.javalin.http.Context
 import io.javalin.http.Handler
 import java.io.PrintStream
 
 fun main(args: Array<String>) {
-    val app = Javalin.create{ config ->
+    Javalin.create { config ->
         config.defaultContentType = "application/json"
         config.enableCorsForAllOrigins()
-    }.start(7000)
-    app.get("/", HelloWorldHandler)
-    app.get("/run/:pgmName", RunRPGHandler)
-}
-
-object HelloWorldHandler : Handler {
-    override fun handle(ctx: Context) {
-        ctx.jsonResponse("Hello world!")
     }
+        .get("/", helloWorld)
+        .get("/run/:pgmName", rpgHandler)
+        .start(7000)
 }
 
-object RunRPGHandler : Handler {
-    override fun handle(ctx: Context) {
-        ctx.jsonResponse(rpgExecution(ctx.pathParam("pgmName")))
-    }
+val helloWorld = fun(ctx: Context) {
+    ctx.jsonResponse("Hello world!")
 }
 
-private fun rpgExecution(pgmName: String): List<String> {
+val rpgHandler = fun(ctx: Context) {
+    ctx.jsonResponse(rpgExecution(ctx.pathParam("pgmName"), extractNumberedParametersFrom(ctx)))
+}
+
+fun extractNumberedParametersFrom(ctx: Context): List<String> =
+    (1..9).mapNotNull { ctx.queryParam("p$it") }
+
+private fun rpgExecution(pgmName: String, parms: List<String>): List<String> {
     val logOutputStream = StringOutputStream()
-    val printStream = PrintStream(logOutputStream)
-    val javaSystemInterface = JavaSystemInterface(printStream)
+    val javaSystemInterface = JavaSystemInterface(PrintStream(logOutputStream))
     val pgm = getProgram("srcRPG/$pgmName", javaSystemInterface)
-    pgm.singleCall(listOf("Franco"))
-    return logOutputStream.toString().lines()
+    pgm.singleCall(parms)
+    return logOutputStream.toString().lines().dropLast(1)
 }
 
 fun Context.jsonResponse(obj: Any) {
-    val gson = GsonBuilder().setPrettyPrinting().create()
-    this.result(gson.toJson(obj))
+    val gSon = GsonBuilder().setPrettyPrinting().create()
+    this.result(gSon.toJson(obj))
 }
-
